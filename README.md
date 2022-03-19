05b7735c5d36140b56c1025301553a6c8bb47752
Cris Zbavitel <criszbav@gmail.com>
2022-03-18 22:48:43 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+Homework 3: Created by Cris Zbavitel
+
+## Quick Start
+### Local Test Setup
+
+First, install a Python 3 virtual environment with:
+```
+sudo apt-get install python3-venv
+```
+
+Create a virtual environment:
+```
+python3 -m venv python_venv
+```
+
+You need to activate the virtual environment when you want to use it:
+```
+source python_venv/bin/activate
+```
+
+To fufil all the requirements for the python server, you need to run:
+```
+pip3 install -r requirements.txt
+```
+Because we are now inside a virtual environment. We do not need sudo.
+
+Then you can start the server with:
+```
+python3 main.py
+```
+
+When the server starts, you can go to your browser and put "127.0.0.1:5000/api/update_fruit_a" or 127.0.0.1:5000/api/unique"
---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+import util
+
+app = Flask(__name__)
+
+username='raywu1990'
+password='test'
+host='127.0.0.1'
+port='5432'
+database='dvdrental'
+
+@app.route("/api/update_basket_a")
+
+def update_basket_a():
+	cursor,connection = util.connect_to_db(username, password, host, port, database)
+	status = util.insert_row(cursor)
+	util.disconnect_from_db(connection,cursor)
+	return render_template('update.html', log_html = status)
+
+@app.route("/api/unique")
+
+def unique():
+	cursor,connection = util.connect_to_db(username, password, host, port, database)
+
+	status, data = util.unique(cursor)
+	col_names = None
+	log = None
+	is_error = data is None
+	if data is not None:
+		col_names = [desc[0] for desc in cursor.description]       
+		log = data[:]
+		
+
+	util.disconnect_from_db(connection,cursor)
+
+	return render_template('unique.html', sql_table = log, table_title = col_names, is_error = is_error)
+
+if __name__ == '__main__':
+	app.debug = True
+	ip = '127.0.0.1'
+	app.run(host=ip)
